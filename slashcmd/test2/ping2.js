cca17dbf32cdcf7cd9844b374167c7bba382b0ff
JsTn <97077253+JsTnasd@users.noreply.github.com>
2022-04-19 15:03:14 -0600

    Update ping2.js

--- slashcmd/test2/ping2.js
+++ slashcmd/test2/ping2.js
@@ -5,6 +5,7 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName('ping2')
     .setDescription('Saldra el ping'),
+    
     run: async(client, interaction) => {
 
         interaction.reply({ content: `Pong! ${client.ws.ping}`})
